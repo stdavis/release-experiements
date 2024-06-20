ced7c299c9b3555b3c1a0def31a802026590fa11
stdavis <stdavis@utah.gov>
2024-06-20 13:06:44 -0600

    fix: bump version number

--- release.config.mjs
+++ release.config.mjs
@@ -13,5 +13,11 @@ export default {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
   ],
 };
